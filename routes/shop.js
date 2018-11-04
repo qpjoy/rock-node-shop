e4786461e9f33d563eabd2c51e6465d4bc2a3c5d
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-11-04 14:44:59 -0600

    :clipboard: new route for home

---
+++ routes/shop.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const path = require('path')
+const rootDir = require('../util/path')
+const router = express.Router()
+router.get('/', (req, res, next) => {
+    res.sendfile(path.join(rootDir, 'views', 'shop.html'))
+})
+module.exports = router
