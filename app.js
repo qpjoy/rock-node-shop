0e6cc23906405ebaeae9e0d563f4125ce9fa2534
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-11-04 14:37:26 -0600

    :clipboard: middleware body parser

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 const app = express()
 
 const port = process.env.PORT || 3000
-
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname,'public')))
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
