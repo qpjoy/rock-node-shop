9dd086b35d2655eec0bf28f6f67fb2b7aa692f4e
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-11-04 14:53:58 -0600

    :clipboard: routes in app.js

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const app = express()
-
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop.js')
 const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname,'public')))
+
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
