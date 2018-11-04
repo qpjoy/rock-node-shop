1b958be12e6b96ae7087387455cc389b8131595c
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-11-04 14:31:38 -0600

    :clipboard: server created with express

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-const port = process.env.PORT || 000
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
     console.log(`Server listening at port: ${port}`)
---
+++ routes/admin.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+router.get('/add-product', (req, res, next) => {
+
+})
+router.post('/add-product', (req, res, next) => {
+
+})
+module.exports = router
