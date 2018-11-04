360ad28782d2cca24b362c1e5debfb72e97f463f
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-11-04 14:36:31 -0600

    :clipboard: page not found created

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
-
+const path = require('path')
 const app = express()
 
 const port = process.env.PORT || 3000
 
+app.use(express.static(path.join(__dirname,'public')))
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
 app.listen(port, () => {
     console.log(`Server listening at port: ${port}`)
 })
