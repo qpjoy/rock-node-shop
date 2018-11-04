9f24c7d389abed426d2606ddf924d94693da61cd
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-11-04 15:15:02 -0600

    :clipboard: sharing data acrosss requests and users

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const path = require('path')
 const rootDir = require('../util/path')
+const adminData = require('./admin')
 const router = express.Router()
 router.get('/', (req, res, next) => {
+    console.log(adminData.products)
     res.sendFile(path.join(rootDir, 'views', 'shop.html'))
 })
 module.exports = router
