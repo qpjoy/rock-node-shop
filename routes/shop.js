7c69c5f96001a32fa7502f4a81a0070cdf97bfc9
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-11-04 15:44:34 -0600

    :clipboard: outpuing dinamyc data with pug

--- routes/shop.js
+++ routes/shop.js
@@ -4,7 +4,6 @@ const rootDir = require('../util/path')
 const adminData = require('./admin')
 const router = express.Router()
 router.get('/', (req, res, next) => {
-    console.log(adminData.products)
-    res.render('shop')
+    res.render('shop', { products: adminData.products, docTitle:'Shop'})
 })
 module.exports = router
