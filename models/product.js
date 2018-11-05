e7185ba87e31f58781356cf4ea73d4a702ce5440
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-11-04 19:59:56 -0600

    saving data in json file

--- models/product.js
+++ models/product.js
@@ -1,12 +1,31 @@
-const products = []
+const fs = require('fs')
+const path = require('path')
+const rootDir = require('../util/path')
 module.exports = class Product {
     constructor(title){
         this.title = title
     }
     save(){
-        products.push(this)
+        const p = path.join(rootDir,'data','products.json')
+        fs.readFile(p, (err, fileContent) => {
+            let products = []
+            if(!err){
+                products = JSON.parse(fileContent)
+            }
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err)
+
+            })
+        })
     }
     static fetchAll(){
-        return products
+        const products = path.join(rootDir, 'data','products.json')
+        fs.readFile(products, (err, fileContent) => {
+            if(err){
+                return []
+            }
+            return JSON.parse(fileContent)
+        })
     }
 }
