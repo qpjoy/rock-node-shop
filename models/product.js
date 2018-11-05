b80fa47c00d2371b50703523ddbf096e2621fba9
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-11-04 20:16:37 -0600

    refactoring code

--- models/product.js
+++ models/product.js
@@ -1,31 +1,31 @@
 const fs = require('fs')
 const path = require('path')
 const rootDir = require('../util/path')
+const p = path.join(rootDir, 'data', 'products.json')
+
+const getProductsFromFile = callback => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            callback([])
+        }
+        else {
+            callback(JSON.parse(fileContent))
+        }
+    })
+}
 module.exports = class Product {
     constructor(title){
         this.title = title
     }
     save(){
-        const p = path.join(rootDir,'data','products.json')
-        fs.readFile(p, (err, fileContent) => {
-            let products = []
-            if(!err){
-                products = JSON.parse(fileContent)
-            }
+        getProductsFromFile(products => {
             products.push(this)
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err)
-
             })
-        })
+        });
     }
     static fetchAll(callback){
-        const products = path.join(rootDir, 'data','products.json')
-        fs.readFile(products, (err, fileContent) => {
-            if(err){
-                callback([])
-            }
-            callback(JSON.parse(fileContent))
-        })
+        getProductsFromFile(callback)
     }
 }
