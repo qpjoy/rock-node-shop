9ac979f0ed7a8428c5961b7bfa811d77be4d12f1
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-11-18 16:56:18 -0600

    :clipboard: app.js modified to sync tables

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const ErrorController = require('./controllers/error')
+const sequelize = require('./util/database')
 const app = express()
 app.set('view engine', 'ejs')
 app.set('views', 'views')
@@ -15,6 +16,12 @@ app.use(express.static(path.join(__dirname,'public')))
 app.use('/admin', adminData.router)
 app.use(shopRoutes)
 app.use(ErrorController.get404)
-app.listen(port, () => {
-    console.log(`Server listening at port: ${port}`)
+sequelize.sync()
+.then(result => {
+    app.listen(port, () => {
+        console.log(`Server listening at port: ${port}`)
+    })
+})
+.catch(err => {
+    console.log(err)
 })
