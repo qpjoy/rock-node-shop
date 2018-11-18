b043d2e34a46335e8f650b0858370e04c0371167
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-11-18 16:44:46 -0600

    :clipboard: installing sequelize and updating connection

--- util/database.js
+++ util/database.js
@@ -1,9 +1,6 @@
-const mysql = require('mysql2')
-
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'nodeuser',
-    password: 'nodeuser',
-    database: 'nodeshop'
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('nodeshop', 'nodeuser','nodeuser',{
+    dialect: 'mysql',
+    host: 'localhost'
 })
-module.exports = pool.promise()
+module.exports = sequelize;
