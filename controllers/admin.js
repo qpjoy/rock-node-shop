6b3b6833c69e14764d3223159a541a5194f8c578
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-11-18 18:04:21 -0600

    :clipboard: using relations

--- controllers/admin.js
+++ controllers/admin.js
@@ -31,8 +31,9 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect('/')
     };
     const productId = req.params.id;
-    Product.findByPk(productId)
-    .then(product => {
+    req.user.getProducts({where: { id: productId}})
+    .then(products => {
+        const product = products[0]
         if(!product){
             return redirect('/');
         }
@@ -73,7 +74,7 @@ exports.postEditProduct = (req, res, next) => {
     })
 }
 exports.getProducts = (req, res, next) => {
-    Product.findAll()
+    req.user.getProducts()
     .then(products => {
         res.render('admin/products', {
             products: products,
