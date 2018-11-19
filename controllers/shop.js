41df35b1a4f0e88e89f65ad5cc7706ae7c761a51
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-11-18 21:14:43 -0600

    :clipboard: removing items from cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -99,9 +99,19 @@ exports.postCart = (req, res, next) => {
 }
 exports.postCartDeleteProduct = (req, res, next) => {
     const productId = req.body.id;
-    Product.findById(productId, product => {
-        Cart.deleteProduct(productId, product.price)
-        res.redirect('/cart')
+    req.user.getCart()
+    .then( cart => {
+        return cart.getProducts({where : { id: productId }})
+    })
+    .then(products => {
+        const product = products[0]
+        return product.cartItem.destroy()
+    })
+    .then(result => {
+        return res.redirect('/cart')
+    })
+    .catch(err => {
+        console.log(err)
     })
 
 }
