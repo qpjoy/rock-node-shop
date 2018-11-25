a7be63ccf0183cfe6ee7688baa0f59bb0f44b7a6
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-11-25 15:37:43 -0600

    :clipboard: creating the schema product

--- models/product.js
+++ models/product.js
@@ -1,55 +1,22 @@
-// const mongodb = require('mongodb')
-// const getDb = require('../util/database').getDb;
-// class Product{
-//     constructor(title, price, description, imageUrl, id, userId){
-//         this.title = title;
-//         this.price = price;
-//         this.description = description;
-//         this.imageUrl = imageUrl;
-//         this._id = id ? new mongodb.ObjectId(id) : null;
-//         this.userId = userId;
-//     }
-//     save(){
-//         const db = getDb();
-//         let opDb;
-//         if(this._id){
-//             opDb = db.collection('products').updateOne({ _id: this._id }, { $set: this });
-//         }
-//         else {
-//             opDb = db.collection('products').insertOne(this);
-//         }
-//         return opDb
-//         .then(product => {
-//             console.log(product);
-//         })
-//         .catch(err => {
-//             console.log(err);
-//         })
+const mongoose = require('mongoose')
 
-//     }
-//     static fetchAll(){
-//         const db = getDb();
-//         return db.collection('products').find().toArray()
-//         .then(products => {
-//             return products;
-//         })
-//         .catch(err => {
-//             console.log(err);
-//         })
-//     }
-//     static findById(id){
-//         const db = getDb();
-//         return db.collection('products').find({ _id: new mongodb.ObjectId(id) }).next()
-//         .then(product => product)
-//         .catch(err => console.log(err))
-//     }
-//     static deleteById(id){
-//         const db = getDb();
-//         return db.collection('products').deleteOne({ _id : new mongodb.ObjectId(id) })
-//         .then(result => {
-//             console.log(result);
-//         })
-//         .catch(err => console.log(err))
-//     }
-// }
-// module.exports = Product;
+const ProductSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    price: {
+        type: Number,
+        require: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageUrl:{
+        type: String,
+        required: true
+    }
+})
+module.exports = mongoose.model('Product', ProductSchema)
