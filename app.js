0415a9364943a9372ba62263dd7c4d5aaaaf82c0
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-11-25 16:03:24 -0600

    :clipboard: adding and using user model

--- app.js
+++ app.js
@@ -2,29 +2,29 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const ErrorController = require('./controllers/error');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const User = require('./models/user');
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 
-const adminData = require('./routes/admin')
-const shopRoutes = require('./routes/shop.js')
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop.js');
 const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'public')));
 
-// app.use((req, res, next) => {
-//     User.findById("5bfa31ec93639f4d1467cc14")
-//     .then(user => {
-//         req.user = new User(user.name, user.email, user.cart, user._id);
-//         console.log(req.user)
-//         next();
-//     })
-//     .catch(err => {
-//         console.log(err);
-//     })
-// })
+app.use((req, res, next) => {
+    User.findById("5bfb1bad935d1a2f0a836583")
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => {
+        console.log(err);
+    })
+})
 
 app.use('/admin', adminData.router)
 app.use(shopRoutes)
@@ -34,6 +34,7 @@ app.use(ErrorController.get404);
 
 mongoose.connect('mongodb://localhost:27017/nodeshop', { useNewUrlParser: true })
 .then(db => {
+   
     app.listen(port, () => {
         console.log(`Server listening at port: ${port}`);
     })
--- models/user.js
+++ models/user.js
@@ -1,104 +1,27 @@
-// const mongodb = require('mongodb');
-// const getDb = require('../util/database').getDb;
-// class User {
-//     constructor(name, email, cart, id){
-//         this.name = name;
-//         this.email = email;
-//         this.cart = { items: [...cart.items] };
-//         this._id = id ? new mongodb.ObjectId(id) : null;
-//     }
-//     save(){
-//         const db = getDb();
-//         let opDb;
-//         if(this._id){
-            
-//         }
-//         else {
-//             opDb = db.collection('users').insertOne(this);
-//         }
-//         return opDb.then(result => {
-//             console.log(result)
-//         })
-//         .catch(err => {
-//             console.log(err)
-//         })
-//     }
-//     addToCart(product){
-//         const productCartIndex = this.cart.items.findIndex( pc => pc.productId.toString() === product._id.toString());
-//         let newQuantity = 1;
-//         const updatedCartItems = [...this.cart.items];
-//         if(productCartIndex >= 0){
-//             newQuantity = this.cart.items[productCartIndex].quantity + 1;
-//             updatedCartItems[productCartIndex].quantity = newQuantity;
-//         }
-//         else {
-//             updatedCartItems.push({
-//                 productId: new mongodb.ObjectId(product._id),
-//                 quantity: newQuantity
-//             })
-//         }
-//         const updatedCart = { items: updatedCartItems }
-//         const db = getDb();
-//         return db
-//             .collection('users')
-//             .updateOne(
-//                 { _id: this._id },
-//                 {$set: { cart : updatedCart }}
-//             );
+const mongoose = require('mongoose')
 
-//     }
-//     getCart(){
-//         const db = getDb();
-//         const productIds = this.cart.items.map( item => item.productId)
-//         return db.collection('products')
-//             .find({ _id: { $in : productIds } }).toArray()
-//             .then(products => {
-//                 return products.map( pro => {
-//                     return {
-//                         ...pro,
-//                         quantity: this.cart.items.find( p => p.productId.toString() === pro._id.toString()).quantity
-//                     }
-//                 })
-//             })
-//             .catch(err => console.log(err));
-//     }
-//     removeItemFromCart(productId){
-//         const db = getDb();
-//         const updatedCart = this.cart.items.filter( item => item.productId.toString() !== productId.toString())
-//         return db.collection('users').updateOne({ _id: this._id }, { $set: { cart: { items: updatedCart }}})
-//             .then(result => {
-//                 console.log(result);
-//             })
-//             .catch(err => console.log(err));
-//     }
-//     getOrders(){
-//         const db = getDb();
-//         return db.collection('orders').find({ 'user._id': this._id }).toArray();
-//     }
-//     addOrder(){
-//         const db = getDb();
-//         return this.getCart()
-//             .then(products => {
-//                 const order = {
-//                     items: products,
-//                     user: {
-//                         _id: this._id,
-//                         name:this.name
-//                     }
-//                 }
-//                 return db.collection('orders').insertOne(order)
-//             })
-//             .then(result => {
-//                 this.cart = { items: [] };
-//                 return db.collection('users')
-//                     .updateOne({ _id: this._id }, { $set: { cart: { items: [] } } });
-//             })
-//             .catch(err => console.log(err));
-//     }
-//     static findById(id){
-//         const db = getDb();
-//         return db.collection('users').findOne({_id : new mongodb.ObjectId(id)});
-        
-//     }
-// }
-// module.exports = User;
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    cart: {
+        items: [
+            {
+                productId: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    required: true
+                },
+                quantity: {
+                    type: Number,
+                    require: true
+                }
+            }
+        ]
+    }
+})
+module.exports = mongoose.model('User', UserSchema);
