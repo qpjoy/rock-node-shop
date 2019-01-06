008632429ff804aad75be6d7cd246f3eb9bcf1bf
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-01-05 21:37:21 -0600

    :bookmark: clearing the cart after storing the order

--- models/user.js
+++ models/user.js
@@ -52,4 +52,9 @@ UserSchema.methods.removeFromCart = function (productId){
     return this.save();
 }
 
+UserSchema.methods.clearCart = function(){
+    this.cart = { items: []};
+    return this.save();
+}
+
 module.exports = mongoose.model('User', UserSchema);
