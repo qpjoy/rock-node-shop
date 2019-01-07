55635f025b9e1d26ca75b8ec292c7aa9ce9b18f8
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-01-06 21:32:17 -0600

    :bookmark: using mongoDb to store sessions

--- app.js
+++ app.js
@@ -6,8 +6,18 @@ const mongoose = require('mongoose');
 
 const session = require('express-session');
 
+const MongoDbSession = require('connect-mongodb-session')(session);
+
 const User = require('./models/user');
 const app = express();
+
+const MONGODB_URI = 'mongodb://localhost:27017/nodeshop';
+
+const store = new MongoDbSession({
+    uri: MONGODB_URI,
+    collection: 'sessions',
+})
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -17,7 +27,12 @@ const shopRoutes = require('./routes/shop.js');
 
 const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(session({secret: 'my secrete', resave: false, saveUninitialized: false}))
+app.use(session({
+    secret: 'my secrete', 
+    resave: false, 
+    saveUninitialized: false,
+    store: store
+}))
 app.use(express.static(path.join(__dirname,'public')));
 
 app.use((req, res, next) => {
@@ -39,7 +54,7 @@ app.use(ErrorController.get404);
 
 
 mongoose
-.connect('mongodb://localhost:27017/nodeshop', { useNewUrlParser: true })
+.connect(MONGODB_URI, { useNewUrlParser: true })
 .then(db => {
    
     app.listen(port, () => {
