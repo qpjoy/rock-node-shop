cb048a66352851b40a78bed0c10559dd810be611
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-01-06 20:49:17 -0600

    :bookmark: creating the login form

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-
+const authRoutes = require('./routes/auth');
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop.js');
+
 const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'public')));
@@ -23,11 +24,12 @@ app.use((req, res, next) => {
     })
     .catch(err => {
         console.log(err);
-    })
+    });
 })
 
-app.use('/admin', adminData.router)
-app.use(shopRoutes)
+app.use('/admin', adminData.router);
+app.use(authRoutes);
+app.use(shopRoutes);
 app.use(ErrorController.get404);
 
 
---
+++ routes/auth.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const AuthController = require('../controllers/auth');
+
+router.get('/login', AuthController.getLogin);
+
+module.exports = router;
