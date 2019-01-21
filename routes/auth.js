d038014eb193a8091b50cfa88498b96cf91e1454
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-01-20 21:41:50 -0600

    :bookmark: password validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 const router = require('express').Router();
 const AuthController = require('../controllers/auth');
 
@@ -8,7 +8,10 @@ router.post('/login', AuthController.postLogin);
 
 router.get('/signup', AuthController.getSignup);
 
-router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email'), AuthController.postSignup);
+router.post('/signup', [
+    check('email').isEmail().withMessage('Please enter a valid email'),
+    body('password', 'Password needs to has at least 5 characteres').isLength({ min: 5}).isAlphanumeric()
+], AuthController.postSignup);
 
 router.post('/logout', AuthController.postLogout);
 
