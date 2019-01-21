763648fcbdd8433fbb4ab22cf4d0c204e7d933a7
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-01-20 21:49:42 -0600

    :bookmark: validation for confirm password

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,14 @@ router.get('/signup', AuthController.getSignup);
 
 router.post('/signup', [
     check('email').isEmail().withMessage('Please enter a valid email'),
-    body('password', 'Password needs to has at least 5 characteres').isLength({ min: 5}).isAlphanumeric()
+    body('password', 'Password needs to has at least 5 characteres').isLength({ min: 5}).isAlphanumeric(),
+    body('confirmPassword').custom((value, {req}) => {
+        if(value !== req.body.password){
+            console.log('VALUES: ', value, req.body.password)
+            throw new Error('Passwords have to match!')
+        }
+        return true;
+    })
 ], AuthController.postSignup);
 
 router.post('/logout', AuthController.postLogout);
